0ca247c4dacd22d3eb960a3fbbfea09493c13502
Varshini <varshini.231cs204@nitk.edu.in>
2024-10-25 21:26:54 +0530

    final submission

--- test/test-helpers.ts
+++ test/test-helpers.ts
@@ -1,24 +1,25 @@
 // Note this require the NPM libraries imported, including sinon, chai, and sinon-chai. 
 // The sinon.restore() call is necessary due to the use of stubbing.
 import sinon from "sinon";
-import { expect } from "chai";
-
+import * as chai from "chai";
 import sinonChai from "sinon-chai";
-import { ethers as hardhatEthers, waffle } from "hardhat";
-import { Contract, Wallet } from "ethers";
 
+import { ethers } from "hardhat"; // Import ethers from Hardhat
+import { Contract, Signer } from "ethers";
 chai.use(sinonChai);
+
 
 afterEach(() => {
   sinon.restore();
 });
 
-export function deployTestContract(name: string): Promise<Contract> {
-  return hardhatEthers
-    .getContractFactory(name, getTestWallet())
-    .then((contractFactory) => contractFactory.deploy());
+export async function deployTestContract(name: string): Promise<Contract> {
+  const wallet = await getTestWallet(); // Get the test wallet
+  const contractFactory = await ethers.getContractFactory(name, wallet);
+  return contractFactory.deploy();
 }
 
-export function getTestWallet(): Wallet {
-  return waffle.provider.getWallets()[0];
+export async function getTestWallet(): Promise<Signer> {
+  const [wallet] = await ethers.getSigners(); // Get the first signer
+  return wallet; // Return the wallet directly
 }
